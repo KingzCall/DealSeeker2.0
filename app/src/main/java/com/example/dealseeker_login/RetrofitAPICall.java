d58299669c9e0a3ca3aeff1db1b4813c3e9a9c04
KingzCall <malachinata@gmail.com>
2024-05-02 04:39:21 +1200

    Refactoring classes to java from kotlin
    Majority of the code was done in java. Converting kotlin classes to java

---
+++ app/src/main/java/com/example/dealseeker_login/RetrofitAPICall.java
@@ -0,0 +1,10 @@
+package com.example.dealseeker_login;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+public interface RetrofitAPICall {
+    // as we are making a get request specifying annotation as get and adding a url end point to it.
+    @GET("43d590f03930")
+    // on below line specifying the method name which we have to call.
+    Call<ResponseObject> getData();
+}
