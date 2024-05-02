721eb5bb5074ef1eaaf706e59e2450ec5d252031
Kai <110900852+KingzCall@users.noreply.github.com>
2024-05-02 14:43:06 +1200

    Revert "Internet/Network Implementation "

--- app/src/main/java/com/example/dealseeker_login/MainActivity.kt
+++ app/src/main/java/com/example/dealseeker_login/MainActivity.kt
@@ -1,27 +1,26 @@
 package com.example.dealseeker_login
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.dealseeker_login.databinding.ActivityMainBinding
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
         setContentView(binding.root)
 
+
         replaceFragment(Home())
+
+
+
 
         binding.bottomNavigationView.setOnItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
@@ -40,34 +39,16 @@ class MainActivity : AppCompatActivity() {
             }
             true
         }
-
-        // Retrofit implementation
-        val retrofit = Retrofit.Builder()
-            .baseUrl("https://json.extendsclass.com/bin/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-
-        val retrofitAPI = retrofit.create(RetrofitAPICall::class.java)
-
-        val call: Call<ResponseObject> = retrofitAPI.getData()
-
-        call.enqueue(object : Callback<ResponseObject> {
-            override fun onResponse(call: Call<ResponseObject>, response: Response<ResponseObject>) {
-                // Handle response
-                val responseObject = response.body()
-                // You can handle the response object here
-            }
-
-            override fun onFailure(call: Call<ResponseObject>, t: Throwable) {
-                // Handle failure
-            }
-        })
     }
 
-    private fun replaceFragment(fragment: Fragment) {
+
+
+
+    private fun replaceFragment(fragment:Fragment){
+
         val fragmentManager = supportFragmentManager
         val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
         fragmentTransaction.commit()
     }
 }
