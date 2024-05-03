2c94e0b35ff7b351b044d444a2935bba8c046393
ManeeshMaleedu <110063522+ManeeshMaleedu@users.noreply.github.com>
2024-05-03 17:26:26 +1200

    Wishlist Updated

--- app/build/generated/data_binding_base_class_source_out/debug/out/com/example/dealseeker_login/databinding/FragmentWishlistBinding.java
+++ app/build/generated/data_binding_base_class_source_out/debug/out/com/example/dealseeker_login/databinding/FragmentWishlistBinding.java
@@ -4,25 +4,38 @@ package com.example.dealseeker_login.databinding;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewbinding.ViewBinding;
+import androidx.viewbinding.ViewBindings;
 import com.example.dealseeker_login.R;
 import java.lang.NullPointerException;
 import java.lang.Override;
+import java.lang.String;
 
 public final class FragmentWishlistBinding implements ViewBinding {
   @NonNull
-  private final FrameLayout rootView;
+  private final LinearLayout rootView;
 
-  private FragmentWishlistBinding(@NonNull FrameLayout rootView) {
+  @NonNull
+  public final Button addToWishlistButton;
+
+  @NonNull
+  public final RecyclerView wishlistRecyclerView;
+
+  private FragmentWishlistBinding(@NonNull LinearLayout rootView,
+      @NonNull Button addToWishlistButton, @NonNull RecyclerView wishlistRecyclerView) {
     this.rootView = rootView;
+    this.addToWishlistButton = addToWishlistButton;
+    this.wishlistRecyclerView = wishlistRecyclerView;
   }
 
   @Override
   @NonNull
-  public FrameLayout getRoot() {
+  public LinearLayout getRoot() {
     return rootView;
   }
 
@@ -43,10 +56,26 @@ public final class FragmentWishlistBinding implements ViewBinding {
 
   @NonNull
   public static FragmentWishlistBinding bind(@NonNull View rootView) {
-    if (rootView == null) {
-      throw new NullPointerException("rootView");
-    }
+    // The body of this method is generated in a way you would not otherwise write.
+    // This is done to optimize the compiled bytecode for size and performance.
+    int id;
+    missingId: {
+      id = R.id.addToWishlistButton;
+      Button addToWishlistButton = ViewBindings.findChildViewById(rootView, id);
+      if (addToWishlistButton == null) {
+        break missingId;
+      }
 
-    return new FragmentWishlistBinding((FrameLayout) rootView);
+      id = R.id.wishlistRecyclerView;
+      RecyclerView wishlistRecyclerView = ViewBindings.findChildViewById(rootView, id);
+      if (wishlistRecyclerView == null) {
+        break missingId;
+      }
+
+      return new FragmentWishlistBinding((LinearLayout) rootView, addToWishlistButton,
+          wishlistRecyclerView);
+    }
+    String missingId = rootView.getResources().getResourceName(id);
+    throw new NullPointerException("Missing required view with ID: ".concat(missingId));
   }
 }
