a3fcff56800df7030954a22b320f7e4d2548accb
YahyaZaidi <cbm2522@autuni.ac.nz>
2024-05-03 19:06:42 +1200

    Commit - yahya

--- app/src/main/java/com/example/dealseeker_login/MainActivity.kt
+++ app/src/main/java/com/example/dealseeker_login/MainActivity.kt
@@ -12,10 +12,7 @@ import androidx.fragment.app.Fragment
 import com.example.dealseeker_login.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
-
     private lateinit var binding: ActivityMainBinding
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,17 +20,7 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(binding.root)
 
-
-
-
-
-
         replaceFragment(Home())
-
-
-
-
-
 
         binding.bottomNavigationView.setOnItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
@@ -45,7 +32,6 @@ class MainActivity : AppCompatActivity() {
                     // Handle button click on the Phones and GPS page
                     replaceFragment(PhonesAndGPS())
                 }
-
                 else -> {
                     // Handle other menu items if needed
                 }
@@ -54,14 +40,10 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-
-
-
     private fun replaceFragment(fragment: Fragment) {
         val fragmentManager = supportFragmentManager
         val fragmentTransaction = fragmentManager.beginTransaction()
         fragmentTransaction.replace(R.id.frame_layout, fragment)
-        fragmentTransaction.addToBackStack(null) // Add fragment to back stack
         fragmentTransaction.commit()
     }
 }
