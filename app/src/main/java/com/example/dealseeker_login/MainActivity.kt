ce6e82d58abcd40d089896475ebf564fc47325a1
YahyaZaidi <cbm2522@autuni.ac.nz>
2024-05-03 22:54:36 +1200

    commit - yahya

--- app/src/main/java/com/example/dealseeker_login/MainActivity.kt
+++ app/src/main/java/com/example/dealseeker_login/MainActivity.kt
@@ -9,6 +9,7 @@ import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import com.example.dealseeker_login.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -20,11 +21,20 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(binding.root)
 
-        replaceFragment(Home())
+        supportFragmentManager.beginTransaction().
+        replace(R.id.frame_layout, Home())
+            .addToBackStack(null)
+            .commit()
+
 
         binding.bottomNavigationView.setOnItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
-                androidx.appcompat.R.id.home -> replaceFragment(Home())
+                androidx.appcompat.R.id.home -> {
+                    supportFragmentManager.popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE)
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame_layout, Home())
+                        .commit()
+                }
                 R.id.search -> replaceFragment(Search())
                 R.id.profile -> replaceFragment(Profile())
                 R.id.wishlist -> replaceFragment(Wishlist())
@@ -41,9 +51,15 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun replaceFragment(fragment: Fragment) {
-        val fragmentManager = supportFragmentManager
-        val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.frame_layout, fragment)
-        fragmentTransaction.commit()
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.frame_layout, fragment)
+            .addToBackStack(null)
+            .commit()
+
+        //val fragmentManager = supportFragmentManager
+        //val fragmentTransaction = fragmentManager.beginTransaction()
+        //fragmentTransaction.replace(R.id.frame_layout, fragment)
+        //fragmentTransaction.addToBackStack(null) // Add fragment to back stack
+        //fragmentTransaction.commit()
     }
 }
