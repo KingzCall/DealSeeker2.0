41438b85b5342d7d0d5ff68de84b61e6183c7a72
keikimala <chantingmara@gmail.com>
2024-05-05 02:30:39 +1200

    Update PermissionDialog.kt

--- app/src/main/java/com/example/dealseeker_login/PermissionDialog.kt
+++ app/src/main/java/com/example/dealseeker_login/PermissionDialog.kt
@@ -1,8 +1,5 @@
 package com.example.dealseeker_login
 
-//import androidx.compose.material.AlertDialog
-//import androidx.compose.material.Divider
-//import androidx.compose.material.Text
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -27,7 +24,7 @@ fun PermissionDialog(
 ) {
     AlertDialog(
         onDismissRequest = onDismiss,
-        buttons = {
+        confirmButton = {
             Column(
                 modifier = Modifier.fillMaxWidth()
             ) {
